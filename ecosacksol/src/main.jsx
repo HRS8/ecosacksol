91b2bd9ac53975afcb1f93f1b7e4610b9f1da79b
HARSHA <harshasai3789@gmail.com>
2024-07-15 04:11:16 +0000

    chkpnt

--- ecosacksol/src/main.jsx
+++ ecosacksol/src/main.jsx
@@ -9,12 +9,12 @@ import Globe from 'react-globe.gl';
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <ThemeProvider>
-        <div className='block w-screen h-screen bg-backg -z-50'>
+        <div className='block w-screen h-screen bg-[#023020] -z-50'>
           <div className='flex justify-center w-full'>
             <ComplexNavbar className="z-10"/>
           </div>
-          <div>
-            <img src="https://drive.google.com/file/d/1inAr_AGw2qWToJWNXPzTEVtxXTByPT1H/view?usp=sharing" alt="plastic earth"/>
+          <div className="z-1">
+            <img src="/compres2rings.png" alt="plastic earth" className='spinn'/>
           </div>
         </div>
     </ThemeProvider>
